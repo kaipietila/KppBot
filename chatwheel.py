af10b586c23e67cf738bd0fe6b19f2e1addea2af
kaipietila <kai.m.pietila@gmail.com>
2019-10-02 21:28:29 +0300

    Add full chatwheel capability

--- chatwheel.py
+++ chatwheel.py
@@ -1,28 +1,75 @@
+import os
+from pathlib import Path
+
 import discord
 from discord.ext import commands
 
 """
 Dota 2 Chatwheel cog to KppBot. Play chatwheel voice emotes until the sun comes up.
 """
-    
+EMOTE_DIR = Path(__file__).resolve().parent.joinpath('emotes')
+
+def find_emote(keyword):
+    emotes = [f for f in os.listdir(EMOTE_DIR)]
+    for emote in emotes:
+        if keyword in emote:
+            return emote
+    else:
+        return None
+
+def find_and_play(voice, keyword):
+    emote = find_emote(keyword)
+    if emote:
+        emote_path = os.path.join(EMOTE_DIR, emote)
+        voice.play(discord.FFmpegPCMAudio(emote_path))
+    else:
+        raise ValueError
+
 class Chatwheel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def play(self, ctx, emote):
-        pass
+    async def play(self, ctx, keyword):
+        voice = ctx.voice_client
+        if voice and voice.is_connected():
+            try:
+                find_and_play(voice, keyword)
+            except ValueError:
+                await ctx.send(f'Did not find anything with {keyword}')
+        else:    
+            channel = ctx.message.author.voice.channel
+            if channel is None:
+                await ctx.send('Your are not in a voice channel!')
+                return False
+            await channel.connect()
+            voice = ctx.voice_client
+            try:
+                find_and_play(voice, keyword)
+            except ValueError:
+                await ctx.send(f'Did not find anything with {keyword}')
+    
+    @commands.command(name="emotes")
+    async def emotes(self, ctx):
+        emotes = [f for f in os.listdir(EMOTE_DIR)]
+        for emote in emotes:
+            await ctx.send(f'{emote} available to play')
 
     @commands.command(name="join")
     async def join(self, ctx):
-        client = ctx.guild.voice_client
-        await client.connect()
+        channel = ctx.message.author.voice.channel
+        if channel is None:
+            await ctx.send('Your are not in a voice channel!')
+            return False
+        await channel.connect()
 
     @commands.command()
     async def leave(self, ctx):
-        server = ctx.message.server
-        voice_channel = self.bot.voice_channel_in(server)
-        await voice_channel.disconnect()
+        voice = ctx.voice_client
+        if voice and voice.is_connected():
+            await voice.disconnect()
+        else:
+            await ctx.send('Cant leave channel. sorry!')
         
 def setup(bot):
     bot.add_cog(Chatwheel(bot))
